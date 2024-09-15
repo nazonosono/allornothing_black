26273be3e77ce25bcc058cb22fd89b64f474bf01
probabilityhill <probabilityhill.rubedo@gmail.com>
2024-09-15 23:24:58 +0900

    濁点、半濁点、小文字、長音符対応、

--- src/js/constant.js
+++ src/js/constant.js
@@ -10,6 +10,31 @@ const HIRAGANA = [
   'る', 'れ', 'ろ', 'わ', 'を', 'ん',
 ];
 
+const HIRAGANA_VOICED_TARGET = [
+  'か', 'き', 'く', 'け', 'こ', 'さ', 'し', 'す', 'せ', 'そ',
+  'た', 'ち', 'つ', 'て', 'と', 'は', 'ひ', 'ふ', 'へ', 'ほ',
+]
+const HIRAGANA_VOICED = [
+  'が', 'ぎ', 'ぐ', 'げ', 'ご', 'ざ', 'じ', 'ず', 'ぜ', 'ぞ',
+  'だ', 'ぢ', 'づ', 'で', 'ど', 'ば', 'び', 'ぶ', 'べ', 'ぼ',
+];
+
+const HIRAGANA_SEMI_VOICED_TARGET = [
+  'は', 'ひ', 'ふ', 'へ', 'ほ',
+]
+
+const HIRAGANA_SEMI_VOICED = [
+  'ぱ', 'ぴ', 'ぷ', 'ぺ', 'ぽ',
+]
+
+const HIRAGANA_SMALL_TARGET = [
+  'あ', 'い', 'う', 'え', 'お', 'つ', 'や', 'ゆ', 'よ',
+]
+
+const HIRAGANA_SMALL = [
+  'ぁ', 'ぃ', 'ぅ', 'ぇ', 'ぉ', 'っ', 'ゃ', 'ゅ', 'ょ',
+]
+
 const CLASS = {
 };
 
@@ -20,4 +45,8 @@ const ID = {
   MODAL_HIRAGANA: 'modal_contents_1',
   INPUT_TEXT: 'input-text',
   ERROR_MESSAGE: 'error-message',
+  VOICED_MARK: 'voiced-mark',
+  SEMI_VOICED_MARK: 'semi-voiced-mark',
+  TO_SMALL: 'to-small-hiragana',
+  DELETE_CHAR: 'delete-char',
 };
--- src/js/script.js
+++ src/js/script.js
@@ -41,10 +41,77 @@ function addToUnlocked(hiraIdx) {
 }
 
 /**
- * 入力された文字を表示する
- * @param {number} hiraIdx
+ * 押下された文字を入力する
+ * @param {String} inputChar
  */
 function typeChar(inputChar) {
   const inputTextElm = document.getElementById(ID.INPUT_TEXT);
   inputTextElm.textContent += inputChar;
+  toggleButtonState(inputChar);
+}
+
+/**
+ * 濁点を付ける
+ */
+function addVoicedMark() {
+  const inputTextElm = document.getElementById(ID.INPUT_TEXT);
+  const inputText = inputTextElm.textContent;
+  const lastChar = inputText.slice(-1);
+  const voicedHiraIdx = HIRAGANA_VOICED_TARGET.indexOf(lastChar);
+  const voicedHira = HIRAGANA_VOICED[voicedHiraIdx];
+  inputTextElm.textContent = inputText.slice(0, -1) + voicedHira;
+  toggleButtonState(voicedHira);
+}
+
+/**
+ * 半濁点を付ける
+ */
+function addSemiVoicedMark() {
+  const inputTextElm = document.getElementById(ID.INPUT_TEXT);
+  const inputText = inputTextElm.textContent;
+  const lastChar = inputText.slice(-1);
+  const semiVoicedHiraIdx = HIRAGANA_SEMI_VOICED_TARGET.indexOf(lastChar);
+  const semiVoicedHira = HIRAGANA_SEMI_VOICED[semiVoicedHiraIdx];
+  inputTextElm.textContent = inputText.slice(0, -1) + semiVoicedHira;
+  toggleButtonState(semiVoicedHira);
+}
+
+/**
+ * 小文字にする
+ */
+function toSmallHiragana() {
+  const inputTextElm = document.getElementById(ID.INPUT_TEXT);
+  const inputText = inputTextElm.textContent;
+  const lastChar = inputText.slice(-1);
+  const smallHiraIdx = HIRAGANA_SMALL_TARGET.indexOf(lastChar);
+  const smallHira = HIRAGANA_SMALL[smallHiraIdx];
+  inputTextElm.textContent = inputText.slice(0, -1) + smallHira;
+  toggleButtonState(smallHira);
+}
+
+/**
+ * 1文字取り消す
+ */
+function deleteChar() {
+  const inputTextElm = document.getElementById(ID.INPUT_TEXT);
+  const inputText = inputTextElm.textContent;
+  const deletedText = inputText.slice(0, -1);
+  inputTextElm.textContent = deletedText;
+  const lastChar = deletedText.slice(-1);
+  toggleButtonState(lastChar);
+}
+
+/**
+ * 入力された文字によってボタンの活性・非活性を切り替える
+ * @param {String} inputChar
+ */
+function toggleButtonState(inputChar) {
+  // 入力文字があれば取消ボタンを活性に、そうでなければ非活性に
+  document.getElementById(ID.DELETE_CHAR).disabled = inputChar ? false : true;
+  // 濁音付与対象であれば濁音ボタンを活性に、そうでなければ非活性に
+  document.getElementById(ID.VOICED_MARK).disabled = HIRAGANA_VOICED_TARGET.includes(inputChar) ? false : true;
+  // 半濁音付与対象であれば半濁音ボタンを活性に、そうでなければ非活性に
+  document.getElementById(ID.SEMI_VOICED_MARK).disabled = HIRAGANA_SEMI_VOICED_TARGET.includes(inputChar) ? false : true;
+  // 小文字変換対象であれば小文字ボタンを活性に、そうでなければ非活性に
+  document.getElementById(ID.TO_SMALL).disabled = HIRAGANA_SMALL_TARGET.includes(inputChar) ? false : true;
 }
